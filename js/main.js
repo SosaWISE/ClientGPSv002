156205aa1e4c192c4a1d820209783bb7af42ca2d
Andres Sosa <SosaWISE@gmail.com>
2013-04-17 14:24:59 -0600

    Adding new files
    Adding new files.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var oViewModel = {
+	LoginTitle: ko.observable("Login"),
+	PersonAge: ko.observable(43),
+	userName: ko.observable("SosaWISE"),
+	userPassword: ko.observable("GetThis")
+};
+
+oViewModel.LoginTitle.subscribe(function(newValue){
+	alert("The Login Title has changed to : " + newValue);
+});
