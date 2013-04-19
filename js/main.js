55148e474a92dd925f815afac984eb700f3c93e3
Austin Condiff <austin.condiff@gmail.com>
2013-04-19 14:27:57 -0600

    Added index and implemented module structure

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ var oViewModel = {
 oViewModel.LoginTitle.subscribe(function(newValue){
 	alert("The Login Title has changed to : " + newValue);
 });
+
+$(function() {
+
+	var currentModule = 'home';
+	$('body').addClass(currentModule);
+	$('nav.primary .indicator').css('top',$('nav.primary a.'+currentModule).offset().top + 15);
+
+	$('nav.primary a').click(function() {
+		$('body').removeClass(currentModule);
+		currentModule = $(this).attr('class');
+		$('nav.primary .indicator').css('top',$(this).offset().top + 15);
+		if(currentModule == 'logo') {
+			$('nav.primary .indicator').css('top',$('nav.primary a.home').offset().top + 15);
+			currentModule = 'home';
+		}
+		$('body').addClass(currentModule);
+	});
+});
