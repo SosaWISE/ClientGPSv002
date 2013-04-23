bb5f8e4acad2af6d3c258c5da99619a9d9683377
Andres Sosa <SosaWISE@gmail.com>
2013-04-23 17:35:32 -0600

    Added new CORS class
    This is trying to abstract the functionality of various differences
    between browsers.

---
+++ js/login.js
@@ -0,0 +1,73 @@
+var oViewModel = {
+	LoginTitle: ko.observable("Login"),
+	PersonAge: ko.observable(43),
+	userName: ko.observable("SosaWISE"),
+	userPassword: ko.observable("GetThis")
+};
+
+oViewModel.LoginTitle.subscribe(function(newValue){
+	alert("The Login Title has changed to : " + newValue);
+});
+
+
+function TestSosStart()
+{
+	var oData = {};
+	oData.AppToken = 'SSE_MAIN_PORTAL';
+
+	var jxHdr = $.ajax({
+		//url: 'http://localhost:50475/api/AuthSrv'
+		//url: 'http://localhost:61023/api/Values'
+		//url: 'http://localhost:61023/AuthSrv/PostStartSSE'
+		//url: 'http://localhost:50475/AuthSrv/SosStart'
+		url: 'http://sse.services.cors/AuthSrv/SosStart'
+		, crossDomain: true
+		, async: true
+		, cache: false
+		, data: JSON.stringify(oData)
+		, type: "POST"
+		, dataType: "json"
+		, contentType: 'application/json; charset=utf-8'
+		, success: fxSuccess
+		, error: fxFailure
+	});
+
+	/** Return handler. */
+	return jxHdr;
+}
+
+function fxSuccess(response)
+{
+	alert('Success.  Session ID: ' + response.SessionId);
+}
+
+function fxFailure(response)
+{
+	alert('Failure: ' + response.statusText);
+	console.log(response);
+}
+
+/** Make sure we are using the right Request object based on the browser. */
+if ('XDomainRequest' in window && window.XDomainRequest !== null) {
+ 
+  // override default jQuery transport
+  jQuery.ajaxSettings.xhr = function() {
+      try 
+      { 
+		oXhr = new XDomainRequest(); 
+		if (oXhr)
+		{
+			oXhr.onerror     = fxFailure;
+			oXhr.onload      = fxSuccess;
+		}
+
+
+      	return oXhr;
+      }
+      catch(e) { }
+  };
+ 
+  // also, override the support check
+  jQuery.support.cors = true;
+ 
+}
---
+++ js/services/CORSRequest.js
@@ -0,0 +1,55 @@
+/**********************************************************************************************************************
+ * @fileOverview Created by JetBrains WebStorm.
+ * Date: 4/23/2013
+ * Time: 04:37 PM
+ * @author: <a href="mailto:asosa@securitysciences.com">Andres Sosa</a>
+ * @description This file contains the definition and other elements that will allow us to be able to create a CORS
+ * request independent of the browser the user is using.
+ * <br /><strong>Dependecies:</strong>  The browser the user is using is one that supports CORS.  And also is a latest
+ * version.
+ *
+ /********************************************************************************************************************/
+ // ** Make sure we are using the right NameSpace.
+SSE = SSE || {};
+SSE.Services = SSE.Services || {};
+
+/**
+ * Creates a new CORS request object.
+ * @class Represents a CORS Request object.
+ */
+(function (CORSRequest, $, undefined) {
+	/** START Private properties. */
+	/**   END Private properties. */
+
+	/** START Public Method. */
+	//noinspection JSValidateJSDoc
+	/**
+	 * @description This class will allow is to create instances of the CORS Request object.
+	 * @param method
+	 * @param url
+	 * @returns {XMLHttpRequest|XDomainRequest} Depends on the browser
+	 */
+	CORSRequest.createNew = function (method, url) {
+		/** Initialize */
+		var xhr = new XMLHttpRequest();
+
+		/** Check if the XMLHttpRequest object has a "withCredentials" property.
+		 * "withCredentials" only exits on the XMLHttpRequest2 object. */
+		if ("withCredentials" in xhr) {
+			xhr.open(method, url, true);
+		} else if (typeof XDomainRequest != "undefined") {
+			/** Otherwise, check if XDomainRequest.
+			 * XDomainRequest only exists in IE, and is IE's way of making CORS requests. */
+			xhr = new XDomainRequest();
+			xhr.open(method, url);
+		} else {
+			/** Otherwise CORS is not supported in this browser. */
+			xhr = null;
+		}
+
+		/** Return result. */
+		return xhr;
+	};
+	/**   END Public Method. */
+
+}( SSE.Services.CORSRequest = SSE.Services.CORSRequest || {}, JQuery ));
