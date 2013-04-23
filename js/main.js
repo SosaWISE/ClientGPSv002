4403fe02b65a7f9d03ea3a38ab4e7907e7ded39f
Andres Sosa <SosaWISE@gmail.com>
2013-04-22 19:15:19 -0600

    Fixing issues.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ oViewModel.LoginTitle.subscribe(function(newValue){
 	alert("The Login Title has changed to : " + newValue);
 });
 
+<<<<<<< HEAD
 $(function() {
 
 	var currentModule = 'home';
@@ -26,3 +27,32 @@ $(function() {
 		$('body').addClass(currentModule);
 	});
 });
+=======
+function TestSosStart()
+{
+	var oData = {};
+	oData.szApplicationToken = 'SSE_MAIN_PORTAL';
+
+	var jxHdr = $.ajax({
+		//url: 'http://localhost:50475/api/AuthSrv'
+		url: 'http://localhost:61023/api/Values'
+		, data: JSON.stringify(oData)
+		, type: "POST"
+		, dataType: "json"
+		, contentType: 'application/json; charset=utf-8'
+		, success: fxSuccess
+		, error: fxFailure
+	});
+}
+
+function fxSuccess(response)
+{
+	alert('Success: ' + response);
+}
+
+function fxFailure(response)
+{
+	alert('Failure: ' + response);
+	console.log(response);
+}
+>>>>>>> Added the Ajax calls
