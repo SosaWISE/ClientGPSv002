369d95f9684c1cec6914028e4fee23baba75f927
Andres Sosa <ASosa@SecuritysSciences.com>
2013-05-13 11:23:53 -0600

    Resolved conflic.

--- js/login.js
+++ js/login.js
@@ -23,6 +23,30 @@ namespace('SSE');
 	 * @private
 	 */
 	var _authCustomer = null;
+
+	/**
+	 *
+	 * @type {{lines: number, length: number, width: number, radius: number, corners: number, rotate: number, direction: number, color: string, speed: number, trail: number, shadow: boolean, hwaccel: boolean, className: string, zIndex: number, top: string, left: string}}
+	 * @private
+	 */
+	var _spinnerOpts = {
+		lines: 9, // The number of lines to draw
+		length: 0, // The length of each line
+		width: 8, // The line thickness
+		radius: 14, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 0, // The rotation offset
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		color: '#000', // #rgb or #rrggbb
+		speed: 1, // Rounds per second
+		trail: 60, // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: 'auto', // Top position relative to parent in px
+		left: 'auto' // Left position relative to parent in px
+	};
 	/**   END Private Properties. */
 
 	/** START Private event handlers. */
@@ -49,6 +73,9 @@ namespace('SSE');
 	});
 	Object.defineProperty(Main, 'AuthCustomer', {
 		get: function() { return _authCustomer; }
+	});
+	Object.defineProperty(Main, 'SpinnerOpts', {
+		get: function () {return _spinnerOpts; }
 	});
 	/**   END Public Properties. */
 
--- js/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: sosawise
- * Date: 5/13/13
- * Time: 11:12 AM
- * To change this template use File | Settings | File Templates.
- */
